cfd301807d0809f8ce4de47da0b543a19fa9d898
Marc Wood <metamarcdw@gmail.com>
2018-03-16 16:29:54 -0500

    Add an App.py module to the mix.

---
+++ App.py
@@ -0,0 +1,14 @@
+React = require("react")
+
+from components.mycomponent import MyComponent
+from components.mycomponent2 import MyComponent2
+
+def App():
+    return __pragma__("xtrans",
+                                "npx babel --presets=react",
+                                "{}", """
+        <div>
+            <MyComponent />
+            <MyComponent2 />
+        </div>
+                                """.strip())
--- index.py
+++ index.py
@@ -1,16 +1,12 @@
 React = require("react")
 ReactDOM = require("react-dom")
 
-from components.mycomponent import MyComponent
-from components.mycomponent2 import MyComponent2
+from App import App
 
 root_component = __pragma__("xtrans",
                             "npx babel --presets=react",
                             "{}", """
-    <div>
-        <MyComponent />
-        <MyComponent2 />
-    </div>
+    <App />
                             """.strip())
 container = document.getElementById("root")
 ReactDOM.render(root_component, container)
