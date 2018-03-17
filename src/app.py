404254982bc44bd63180cd14b1e77973fef7dbd9
Marc Wood <metamarcdw@gmail.com>
2018-03-16 23:43:44 -0500

    Make App a class-based Component.

--- src/app.py
+++ src/app.py
@@ -1,14 +1,16 @@
 React = require("react")
 
+from components.component import Component
 from components.mycomponent import MyComponent
 from components.mycomponent2 import MyComponent2
 
-def App():
-    return __pragma__("xtrans",
-                      "npx babel --presets=react",
-                      "{}", """
-        <div>
-            <MyComponent />
-            <MyComponent2 />
-        </div>
-                      """.strip())
+class App(Component):
+    def render():
+        return __pragma__("xtrans",
+                          "npx babel --presets=react",
+                          "{}", """
+            <div>
+                <MyComponent />
+                <MyComponent2 />
+            </div>
+                          """.strip())
